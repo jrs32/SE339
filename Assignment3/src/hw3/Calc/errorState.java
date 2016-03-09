aea7945670311f7981134a3ce1c1475cb6013209
Thomas Devens <Thomas Devens>
2016-03-08 19:39:25 -0600

    Maybe doing things

---
+++ Assignment3/src/hw3/Calc/errorState.java
@@ -0,0 +1,24 @@
+package hw3.Calc;
+
+public class errorState extends CalcState
+{
+
+	@Override
+	public void Digit(Calculator calc, char c) {
+		// Does Nothing
+		
+	}
+
+	@Override
+	public void Operation(Calculator calc, char c) {
+		// Does Nothing
+		
+	}
+
+	@Override
+	public void Equal(Calculator calc) {
+		// Does Nothing
+		
+	}
+
+}
